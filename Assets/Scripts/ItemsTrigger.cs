e6964cccd99ffa65fa505bd59869d8a8a8498093
Cylin <130658828+ShujieJ@users.noreply.github.com>
2024-12-29 15:58:21 +0800

    补充注释

--- Assets/Scripts/ItemsTrigger.cs
+++ Assets/Scripts/ItemsTrigger.cs
@@ -6,13 +6,27 @@ using UnityEngine;
 
 public class ItemsTrigger : MonoBehaviour
 {
+    /// <summary>
+    /// 进行各种道具的碰撞检测
+    /// </summary>
+    /// <param name="other"></param>
     public void OnTriggerEnter2D(Collider2D other)
     {
+        // 如果本物体是加速道具，碰撞体是玩家
         if (gameObject.CompareTag("SpeedUp") && other.CompareTag("Player"))
         {
-            PlayerController player = other.GetComponent<PlayerController>();
-            player.speed += Settings.speedUpItemValue;
-            Destroy(gameObject);
+            ItemSpeedUp(other);
         }
+    }
+
+    /// <summary>
+    /// 加速道具
+    /// </summary>
+    /// <param name="playerCollider"></param>
+    private void ItemSpeedUp(Collider2D playerCollider)
+    {
+        PlayerController player = playerCollider.GetComponent<PlayerController>();
+        player.speed += Settings.speedUpItemValue;
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -2,7 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 游戏各项常量设置
+/// </summary>
 public class Settings
 {
+    /// <summary>
+    /// 加速道具的加速值
+    /// </summary>
     public const float speedUpItemValue = 0.5f;
 }
